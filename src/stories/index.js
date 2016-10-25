666a797cc627796d0f3cd5044079637dfbf7faf6
Trevor D. Miller <trevordmiller@icloud.com>
2016-10-25 16:29:54 -0600

    Remove unused storybook directory now that stories are in component folders

--- src/stories/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {storiesOf} from '@kadira/storybook'
-import Logo from '../App/components/Logo'
-
-storiesOf('Logo', module)
-  .add('Default', () => (
-    <Logo />
-  ))
