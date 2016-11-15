9071b41e24554fc55689508df70e1169479ece28
Trevor D. Miller <trevordmiller@icloud.com>
2016-11-15 16:43:18 -0700

    Add lesson state filtering to fakeApi

--- src/App/screens/Instructor/state/epics/fetchLessonsForInstructor.js
+++ src/App/screens/Instructor/state/epics/fetchLessonsForInstructor.js
@@ -1,4 +1,5 @@
 import {Observable} from 'rxjs'
+import devQueryString from 'query-string'
 import parse from 'parse-link-header'
 import * as instructorActionTypes from '../actions/instructorActionTypes'
 import {receiveInstructorLessons} from '../actions'
@@ -13,7 +14,6 @@ function handleLessonsResponse(response) {
   })
 }
 
-// I couldn't figure out how to get the Rx ajax operator to give me the headers so I just used fetch...
 function fetchLessons(lessonPage) {
 
   const paramNamesByEnv = process.env.REACT_APP_FAKE_API
@@ -30,13 +30,17 @@ function fetchLessons(lessonPage) {
     [paramNamesByEnv.page]: lessonPage.page,
     [paramNamesByEnv.size]: lessonPage.size,
     ...(lessonPage.states
-      ? {states: lessonPage.states}
+      ? {state: lessonPage.states}
       : {}
     ),
   }
 
-  const url = `${lessonPage.lessons_url}${createQueryString(params)}`
+  const queryString = process.env.REACT_APP_FAKE_API
+    ? `?${devQueryString.stringify(params)}`
+    : createQueryString(params)
+  const url = `${lessonPage.lessons_url}${queryString}`
 
+  // I couldn't figure out how to get the Rx ajax operator to give me the headers so I just used fetch...
   return Observable.fromPromise(
     fetch(url, {headers})
       .then(handleLessonsResponse))
