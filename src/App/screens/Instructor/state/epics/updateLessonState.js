3b726b75c8f66f9f06f0675596be5e22efa2158c
Trevor D. Miller <trevordmiller@icloud.com>
2016-11-22 21:17:13 -0700

    Observable.ajax PUT working

--- src/App/screens/Instructor/state/epics/updateLessonState.js
+++ src/App/screens/Instructor/state/epics/updateLessonState.js
@@ -1,4 +1,4 @@
-import {ajax} from 'rxjs/observable/dom/ajax'
+import {Observable} from 'rxjs'
 import {STARTED_UPDATE_LESSON_STATE} from '../actions/instructorActionTypes'
 import {endUpdateLessonState} from '../actions'
 import headers from './utils/headers'
@@ -6,7 +6,17 @@ import headers from './utils/headers'
 export default (action$) => (
   action$.ofType(STARTED_UPDATE_LESSON_STATE)
     .switchMap(
-      ({payload}) => ajax.put(payload.lessonUrl, {lesson: {state: payload.newState}}, headers),
-      ({payload}, {lesson}) => endUpdateLessonState({payload, lesson})
+      ({payload}) => Observable.ajax({
+        type: 'PUT',
+        headers,
+        crossDomain: true,
+        url: payload.lessonUrl,
+        body: {
+          state: payload.newState,
+        },
+      }),
+      ({payload}, {lesson}) => (
+        endUpdateLessonState({payload, lesson})
+      )
     )
 )
