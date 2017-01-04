f858876dac8c5b713156901e31bcf358be1dd3f8
Trevor D. Miller <trevordmiller@icloud.com>
2017-01-04 16:11:59 -0700

    Add navigation logo

--- src/App/screens/Instructor/components/Logo/index.js
+++ src/App/screens/Instructor/components/Logo/index.js
@@ -1,11 +1,18 @@
-// TODO: Consume from egghead-ui once published
-
 import React from 'react'
-import logo from './logo.svg'
+import instructorEggo from './instructorEggo.svg'
+import instructorBanner from './instructorBanner.svg'
 
 export default () => (
-  <img
-    src={logo}
-    alt="egghead.io logo"
-  />
+  <div>
+    <img
+      src={instructorEggo}
+      alt='egghead.io instructors logo, eggo only'
+      className='mw2 db dn-ns'
+    />
+    <img
+      src={instructorBanner}
+      alt='egghead.io instructors logo full'
+      className='w-100 dn db-ns'
+    />
+  </div>
 )
