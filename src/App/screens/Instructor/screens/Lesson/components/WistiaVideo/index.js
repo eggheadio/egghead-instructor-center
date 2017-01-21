89b547694b312d7427dd1e725d80137b81304996
Trevor D. Miller <trevordmiller@icloud.com>
2017-01-20 20:12:30 -0700

    Clean up Wistia component

--- src/App/screens/Instructor/screens/Lesson/components/WistiaVideo/index.js
+++ src/App/screens/Instructor/screens/Lesson/components/WistiaVideo/index.js
@@ -1,47 +1,24 @@
 import React, {PropTypes} from 'react'
 
-const WistiaVideo = ({wistiaId}) => {
-
-  const url = `//fast.wistia.net/embed/iframe/${wistiaId}?videoFoam=true`
-  const iframeRefCallback = function iframeRefCallback(iframe) {
-    if (iframe) {
-      iframe.contentWindow.location.replace(url)
-    }
-  }
-
-  return (
-    <div
-      className="wistia_responsive_padding"
-      style={{
-        padding: '56.25% 0 0 0',
-        position: 'relative'
-      }}
-    >
-      <div
-        className="wistia_responsive_wrapper"
-        style={{
-          height: '100%',
-          left: 0,
-          position: 'absolute',
-          top: 0,
-          width: '100%'
+const WistiaVideo = ({wistiaId}) => (
+  <div className='aspect-ratio aspect-ratio--16x9'>
+    <div className='aspect-ratio--object'>
+      <iframe
+        ref={(iframe) => {
+          if (iframe) {
+            iframe.contentWindow.location.replace(`//fast.wistia.net/embed/iframe/${wistiaId}?videoFoam=true`)
+          }
         }}
-      >
-        <iframe
-          ref={iframeRefCallback}
-          allowTransparency="true"
-          frameBorder="0"
-          scrolling="no"
-          className="wistia_embed"
-          name="wistia_embed"
-          allowFullScreen
-          width="100%"
-          height="100%"
-        />
-      </div>
+        width='100%'
+        height='100%'
+        frameBorder='0'
+        scrolling='no'
+        allowTransparency
+        allowFullScreen
+      />
     </div>
-  )
-}
+  </div>
+)
 
 WistiaVideo.propTypes = {
   wistiaId: PropTypes.string.isRequired,
