acd517e856db84de22953bb345bbe20376568d0a
Trevor D. Miller <trevordmiller@icloud.com>
2017-01-26 14:07:28 -0700

    Add source error tracking with context

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import {Provider} from 'react-redux'
+import Honeybadger from 'honeybadger-js'
+import {honeybadgerKey} from './utils/keys'
 import configureStore from './state/'
 import Router from './components/Router'
 
 const store = configureStore()
+
+Honeybadger.configure({
+  api_key: honeybadgerKey,
+  onerror: false,
+})
+
+window.onerror = (message, url, line, column, error) => {
+  const instructorId = store.getState().appScreen.user.instructor_id
+  Honeybadger.setContext({
+    instructorId,
+  })
+  Honeybadger.notify(error ? error : {
+    name: 'window.onerror',
+    message,
+    stack: [message, '\n    at ? (', url || 'unknown', ':', line || 0, ':', column || 0, ')'].join(''),
+  })
+}
+
+if (process.env.NODE_ENV === 'production') {
+}
 
 export default (props) => (
   <Provider store={store}>
