8a419da4857c457a8231e04b6d2039a7813307a7
Trevor D. Miller <trevordmiller@icloud.com>
2017-01-26 20:34:35 -0400

    Error tracking polish (#93)
    * Add source error tracking with context
    
    * Refactor error tracking
    
    * Add error tracking only in prod
    
    * Refactor error tracking naming
    
    *  Add past ten redux actions to error tracking

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import {Provider} from 'react-redux'
+import {initializeErrorTracking} from './utils/errorTracking'
 import configureStore from './state/'
 import Router from './components/Router'
 
 const store = configureStore()
+
+if (process.env.NODE_ENV === 'production') {
+  initializeErrorTracking(store)
+}
 
 export default (props) => (
   <Provider store={store}>
---
+++ src/App/utils/errorTracking.js
@@ -0,0 +1,23 @@
+import Honeybadger from 'honeybadger-js'
+import {honeybadgerKey} from './keys'
+
+export const initializeErrorTracking = (store) => {
+  
+  Honeybadger.configure({
+    api_key: honeybadgerKey,
+    onerror: false,
+  })
+
+  window.onerror = (message, url, line, column, error) => {
+    const instructorId = store.getState().appScreen.user.instructor_id
+    Honeybadger.setContext({
+      instructorId,
+      precedingActions: JSON.parse(localStorage.getItem('precedingActions')),
+    })
+    Honeybadger.notify(error ? error : {
+      name: message,
+      message,
+      stack: [message, '\n    at ? (', url || 'unknown', ':', line || 0, ':', column || 0, ')'].join(''),
+    })
+  }
+}
