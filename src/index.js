2bba118a8891e9f2f301710114eecf4976757220
Trevor D. Miller <trevordmiller@icloud.com>
2017-01-27 14:27:17 -0700

    Move App directory contents to root and remove App directory

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import 'rxjs'
 import 'tachyons'
 import 'font-awesome/css/font-awesome.min.css'
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {initializeErrorTracking} from './utils/errorTracking'
+import configureStore from './state/'
+import Router from './components/Router'
+
+const store = configureStore()
+
+if (process.env.NODE_ENV === 'production') {
+  initializeErrorTracking(store)
+}
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router />
+  </Provider>,
   document.getElementById('root')
 )
