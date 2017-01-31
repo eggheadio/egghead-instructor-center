6a0e500a08fd78cb15b6bd1595ce9c5518e850a6
Trevor D. Miller <trevordmiller@icloud.com>
2017-01-31 15:52:31 -0700

    Refactor instructors to use fetch component

--- src/screens/Instructors/index.js
+++ src/screens/Instructors/index.js
@@ -1,54 +1,28 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
+import React from 'react'
 import {filter, reject} from 'lodash'
 import {instructorPulseTitleText, unpublishedTitleText, publishedTitleText} from 'utils/text'
-import Loading from 'components/Loading'
+import Fetch from 'components/Fetch'
 import Split from 'components/Split'
-import {startFetchInstructors} from './state/actions'
 import InstructorsList from './components/InstructorsList'
 
-export default connect(
-  ({instructorsScreen}) => ({
-    instructors: instructorsScreen.instructors,
-  }),
-  {
-    startFetchInstructors,
-  }
-)(class Admin extends Component {
-
-  componentDidMount() {
-
-    const {instructors, startFetchInstructors} = this.props
-
-    if(!instructors) {
-      startFetchInstructors()
-    }
-  }
-
-  render() {
-    
-    const {instructors} = this.props
-
-    if(!instructors) {
-      return <Loading />
-    }
-
-    return (
+export default () => (
+  <Fetch path='/api/v1/instructors'>
+    {({data}) => (
       <Split
         title={instructorPulseTitleText}
         main={
           <InstructorsList
             title={unpublishedTitleText}
-            instructors={filter(instructors, ['published_lessons', 0])}
+            instructors={filter(data, ['published_lessons', 0])}
           />
         }
         aside={
           <InstructorsList
             title={publishedTitleText}
-            instructors={reject(instructors, ['published_lessons', 0])}
+            instructors={reject(data, ['published_lessons', 0])}
           />
         }
       />
-    )
-  }
-})
+    )}
+  </Fetch>
+)
