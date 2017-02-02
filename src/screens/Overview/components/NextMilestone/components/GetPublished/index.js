e94ab3eab9b8550bf2593fedb30c94c1da580053
Trevor D. Miller <trevordmiller@icloud.com>
2017-02-02 09:59:15 -0700

    Remove redux pieces from get published

--- src/screens/Overview/components/NextMilestone/components/GetPublished/index.js
+++ src/screens/Overview/components/NextMilestone/components/GetPublished/index.js
@@ -1,32 +1,17 @@
 import React from 'react'
 import {getPublishedTitleText} from 'utils/text'
-import Split from 'components/Split'
-import LessonsByPage from '../../components/LessonsByPage'
-import Intro from '../../components/Intro'
+import Heading from 'components/Heading'
 import GetPublishedSteps from './components/GetPublishedSteps'
 import GetPublishedFaq from './components/GetPublishedFaq'
 
 export default ({
   instructor,
-  lessonPage,
 }) => (
-  <LessonsByPage instructor={instructor}>
-    {({currentPage}) => (
-      <Split
-        intro={
-          <Intro instructor={instructor} />
-        }
-        title={getPublishedTitleText}
-        main={
-          <GetPublishedSteps
-            instructor={instructor}
-            lessonPage={lessonPage}
-          />
-        }
-        aside={
-          <GetPublishedFaq />
-        }
-      />
-    )}
-  </LessonsByPage>
+  <div>
+    <Heading level='3'>
+      {getPublishedTitleText}
+    </Heading>
+    <GetPublishedSteps instructor={instructor} />
+    <GetPublishedFaq />
+  </div>
 )
