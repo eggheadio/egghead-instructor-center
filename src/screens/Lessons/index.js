f84ea2e44d4241d1e5ab86c66001823a2f082531
Trevor D. Miller <trevordmiller@icloud.com>
2017-03-09 16:59:57 -0700

    Convert lessons screen to widgets and lists

--- src/screens/Lessons/index.js
+++ src/screens/Lessons/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
-import {lessonsTitleText} from 'utils/text'
-import Screen from 'components/Screen'
 import LessonListsByStates from 'components/LessonListsByStates'
 
 export default () => (
-  <Screen
-    title={lessonsTitleText}
-    main={
-      <LessonListsByStates />
-    }
-  />
+  <LessonListsByStates />
 )
