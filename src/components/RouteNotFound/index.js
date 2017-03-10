c8793e11f6f1e8e8a241d5c81a0649e52be487a7
Trevor D. Miller <trevordmiller@icloud.com>
2017-03-09 20:59:50 -0700

    Remove Screen component

--- src/components/RouteNotFound/index.js
+++ src/components/RouteNotFound/index.js
@@ -1,32 +1,27 @@
 import React from 'react'
-import {Heading} from 'egghead-ui'
+import {Heading, Paragraph} from 'egghead-ui'
 import {
   pageNotFoundTitleText,
   pageNotFoundDescriptionText,
   crackedEggoAltText,
 } from 'utils/text'
 import Main from 'components/Main'
-import Screen from 'components/Screen'
 import crackedEggo from './crackedEggo.png'
 
 export default () => (
   <Main>
-    <Screen
-      title='404'
-      main={
-        <img
-          src={crackedEggo}
-          alt={crackedEggoAltText}
-        />
-      }
-      aside={
-        <div>
-          <Heading level='3'>
-            {pageNotFoundTitleText}
-          </Heading>
-          <p>{pageNotFoundDescriptionText}</p>
-        </div>
-      }
+    <Heading level='1'>
+      404
+    </Heading>
+    <img
+      src={crackedEggo}
+      alt={crackedEggoAltText}
     />
+    <Heading level='3'>
+      {pageNotFoundTitleText}
+    </Heading>
+    <Paragraph>
+      {pageNotFoundDescriptionText}
+    </Paragraph>
   </Main>
 )
