af8010a4ac98fe4d92b9b80e20203c755f679c26
Trevor D. Miller <trevordmiller@icloud.com>
2017-03-28 11:05:53 -0600

    Polish title card component (resolve #199)

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,7 @@
 import React, {PropTypes} from 'react'
 
 const Main = ({children}) => (
-  <main className='bg-white-secondary h-100 w-100 ml7-ns mt4 mt0-ns'>
+  <main className='h-100 w-100 ml7-ns mt4 mt0-ns'>
     <div className='mw8 pv4 ph3 ph4-ns'>
       {children}
     </div>
