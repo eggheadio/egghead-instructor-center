e9ad9360a144e2cca839c7e3e8ca5da10f363f3d
Trevor D. Miller <trevordmiller@icloud.com>
2017-03-30 14:50:39 -0600

    Add line chart abstraction

--- src/screens/Dashboard/components/InstructorRevenueCard/index.js
+++ src/screens/Dashboard/components/InstructorRevenueCard/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {find, size, map} from 'lodash'
 import {Card} from 'egghead-ui'
 import {Text} from 'react-localize'
-import {Line} from 'react-chartjs-2'
 import WrappedRequest from 'components/WrappedRequest'
+import LayoutColumns from 'components/LayoutColumns'
 import currentMonthStartDate from './utils/currentMonthStartDate'
 import totalRevenue from './utils/totalRevenue'
 import removeRevenueMonth from './utils/removeRevenueMonth'
 import RevenuePeriod from './components/RevenuePeriod'
+import LineChart from './components/LineChart'
 
 export default ({revenueUrl}) => revenueUrl
   ? <WrappedRequest
@@ -19,65 +20,56 @@ export default ({revenueUrl}) => revenueUrl
         const currentTotalRevenue = totalRevenue(removeRevenueMonth(data, currentMonthStartDate()))
         const hasCurrentRevenue = size(data) > 0
           && ((currentMonthRevenue && currentMonthRevenue.revenue > 0) || (currentTotalRevenue && currentTotalRevenue.revenue > 0))
-        const currentMonths = map(data, month => month.month)
-        const currentRevenues = map(data, month => month.revenue)
-        const revenueColor = '#4786ff'
-        const revenueColorSecondary = '#386fda'
+        const currentMonthDates = map(data, month => month.month)
+        const currentRevenuePoints = map(data, month => month.revenue)
+        const currentMinutesPoints = map(data, month => month.minutes_watched)
+        const revenueColor = '#4786ff' // # blue
+        const minutesColor = '#171e27' // # base-secondary
 
         if(!hasCurrentRevenue) {
           return null
         }
 
-        const chartData = {
-          labels: currentMonths,
-          datasets: [
-            {
-              label: 'Revenue',
-              fill: false,
-              lineTension: 0.3,
-              backgroundColor: revenueColor,
-              borderColor: revenueColor,
-              borderCapStyle: 'butt',
-              borderDash: [],
-              borderDashOffset: 0.0,
-              borderJoinStyle: 'miter',
-              pointBorderColor: revenueColor,
-              pointBackgroundColor: '#fff',
-              pointBorderWidth: 1,
-              pointHoverRadius: 5,
-              pointHoverBackgroundColor: revenueColor,
-              pointHoverBorderColor: revenueColorSecondary,
-              pointHoverBorderWidth: 2,
-              pointRadius: 3,
-              pointHitRadius: 10,
-              data: currentRevenues,
-            }
-          ]
-        }
-
         return (
           <Card>
-            <div className='flex-ns'>
-              <div className='pa5'>
-                <div className='pb4'>
-                  <RevenuePeriod
-                    title={<Text message='instructorRevenue.currentMonth.title' />}
-                    revenue={currentMonthRevenue.revenue}
-                    subscriberMinutes={currentMonthRevenue.minutes_watched}
-                  />
-                </div>
-                <RevenuePeriod
-                  title={<Text 
-                    message='instructorRevenue.previousMonths.title' 
-                    values={[currentTotalRevenue.monthCount]} 
-                  />}
-                  revenue={currentTotalRevenue.revenue}
-                  subscriberMinutes={currentTotalRevenue.minutes_watched}
+            <div>
+
+              <div className='pa4'>
+                <LayoutColumns 
+                  items={[
+                    <RevenuePeriod
+                      title={<Text 
+                        message='instructorRevenue.previousMonths.title' 
+                        values={[currentTotalRevenue.monthCount]} 
+                      />}
+                      revenue={currentTotalRevenue.revenue}
+                      subscriberMinutes={currentTotalRevenue.minutes_watched}
+                    />,
+                    <RevenuePeriod
+                      title={<Text message='instructorRevenue.currentMonth.title' />}
+                      revenue={currentMonthRevenue.revenue}
+                      subscriberMinutes={currentMonthRevenue.minutes_watched}
+                    />,
+                  ]}
                 />
               </div>
-              <div className='pa4 bg-white-secondary br2'>
-                <Line data={chartData} />
+
+              <div className='pa4 bg-white-secondary br2 br--bottom'>
+                <LineChart
+                  xAxis={currentMonthDates}
+                  yAxis={[
+                    {
+                      color: revenueColor,
+                      points: currentRevenuePoints,
+                    },
+                    {
+                      color: minutesColor,
+                      points: currentMinutesPoints,
+                    },
+                  ]}
+                />
               </div>
+
             </div>
           </Card>
         )
