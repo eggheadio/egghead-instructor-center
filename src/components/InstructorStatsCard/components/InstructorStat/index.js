7fb41cc13b12d8e273569f118da1bb924abf1fbe
Trevor D. Miller <trevordmiller@icloud.com>
2017-04-04 15:48:20 -0600

    Finish adding lesson stats graphics

--- src/components/InstructorStatsCard/components/InstructorStat/index.js
+++ src/components/InstructorStatsCard/components/InstructorStat/index.js
@@ -4,8 +4,8 @@ import Anchor from 'components/Anchor'
 import Image from 'components/Image'
 
 export default ({count, label, graphics}) => (
-  <div className='flex justify-center'>
-    <div className='flex items-center'>
+  <div>
+    <div className='flex items-center justify-center mb3'>
       <div className='base f3 b mr2'>
         {count}
       </div>
@@ -13,15 +13,16 @@ export default ({count, label, graphics}) => (
         {label}
       </div>
     </div>
-    <div>
+    <div className='flex flex-wrap justify-center'>
       {map(graphics, graphic => (
         <Anchor 
-          key={graphic.label}
+          key={graphic.name}
           url={graphic.http_url}
         >
           <Image 
             src={graphic.logo_http_url}
             alt={graphic.label}
+            className='w2 mr3 mb3'
           />
         </Anchor>
       ))}
