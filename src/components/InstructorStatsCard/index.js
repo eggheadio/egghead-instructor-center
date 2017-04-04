9710d15a023f48a7ae313e8075dc17b1b836d8ac
Trevor D. Miller <trevordmiller@icloud.com>
2017-04-04 16:16:25 -0600

    Keep out course graphics until API is fixed

--- src/components/InstructorStatsCard/index.js
+++ src/components/InstructorStatsCard/index.js
@@ -16,32 +16,32 @@ export default ({instructor}) => {
   return (
     <Card>
       <div className='pa5'>
-        <div className='mb5'>
-          <WrappedRequest
-            url={createLessonsUrl({
-              lessons_url: instructor.lessons_url
-            })}
-          >
-            {({data}) => (
+        <WrappedRequest
+          url={createLessonsUrl({
+            lessons_url: instructor.lessons_url
+          })}
+        >
+          {({data}) => (
+            <InstructorStat
+              count={instructor.published_lessons}
+              label={pluralize('Lesson', instructor.published_lessons)}
+              graphics={uniqBy(map(data, lesson => ({
+                ...lesson.technology,
+              })), technology => technology.name)}
+            />
+          )}
+        </WrappedRequest>
+        {/* 
+        <div className='mt5'>
+          <WrappedRequest url={`/api/v1/series?instructor_id=${instructor.id}`}>
+            {({data}) => console.log('data', data) || (
               <InstructorStat
-                count={instructor.published_lessons}
-                label={pluralize('Lesson', instructor.published_lessons)}
-                graphics={uniqBy(map(data, lesson => ({
-                  ...lesson.technology,
-                })), technology => technology.name)}
+                count={instructor.published_courses}
+                label={pluralize('Course', instructor.published_courses)}
               />
             )}
           </WrappedRequest>
         </div>
-        {/* 
-        <WrappedRequest url={`/api/v1/series?instructor_id=${instructor.id}`}>
-          {({data}) => console.log('data', data) || (
-            <InstructorStat
-              count={instructor.published_courses}
-              label={pluralize('Course', instructor.published_courses)}
-            />
-          )}
-        </WrappedRequest>
         */}
       </div>
     </Card>
