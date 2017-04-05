fb07a89dba894b3d500dc9e89ff94ab423585667
Trevor D. Miller <trevordmiller@icloud.com>
2017-04-05 15:35:50 -0600

    Polish course stats (resolve #213)

--- src/components/InstructorStatsCard/index.js
+++ src/components/InstructorStatsCard/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import pluralize from 'pluralize'
 import {map, uniqBy} from 'lodash'
-import {Card} from 'egghead-ui'
-import {hasUnlockedPublished} from 'utils/milestones'
+import {Maybe, Card} from 'egghead-ui'
+import {hasUnlockedPublished, hasUnlockedCoursePublished} from 'utils/milestones'
 import createLessonsUrl from 'utils/createLessonsUrl'
 import WrappedRequest from 'components/WrappedRequest'
 import InstructorStat from './components/InstructorStat'
@@ -26,23 +26,30 @@ export default ({instructor}) => {
               count={instructor.published_lessons}
               label={pluralize('Lesson', instructor.published_lessons)}
               graphics={uniqBy(map(data, lesson => ({
-                ...lesson.technology,
+                name: lesson.technology.name,
+                graphicUrl: lesson.technology.logo_http_url,
+                httpUrl: lesson.technology.http_url,
               })), technology => technology.name)}
             />
           )}
         </WrappedRequest>
-        {/* 
-        <div className='mt5'>
-          <WrappedRequest url={`/api/v1/series?instructor_id=${instructor.id}`}>
-            {({data}) => console.log('data', data) || (
-              <InstructorStat
-                count={instructor.published_courses}
-                label={pluralize('Course', instructor.published_courses)}
-              />
-            )}
-          </WrappedRequest>
-        </div>
-        */}
+        <Maybe condition={hasUnlockedCoursePublished(instructor.published_courses)}>
+          <div className='mt4'>
+            <WrappedRequest url={`/api/v1/series?instructor_id=${instructor.id}`}>
+              {({data}) => (
+                <InstructorStat
+                  count={instructor.published_courses}
+                  label={pluralize('Course', instructor.published_courses)}
+                  graphics={map(data, course => ({
+                    name: course.title,
+                    graphicUrl: course.square_cover_url,
+                    httpUrl: course.http_url,
+                  }))}
+                />
+              )}
+            </WrappedRequest>
+          </div>
+        </Maybe>
       </div>
     </Card>
   )
--- src/utils/milestones/index.js
+++ src/utils/milestones/index.js
@@ -2,6 +2,10 @@ export const hasUnlockedPublished = (publishedLessons) => (
   publishedLessons > 0
 )
 
+export const hasUnlockedCoursePublished = (publishedCourses) => (
+  publishedCourses > 0
+)
+
 export const selfReviewThreshold = 12
 
 export const hasUnlockedSelfReview = (publishedLessons) => (
