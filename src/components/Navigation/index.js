2b4ee22918fa79a615959e8fbaf25e03eddaabd2
Trevor D. Miller <trevordmiller@users.noreply.github.com>
2017-05-01 16:10:45 -0600

    Fix navigation bug (#225)

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,14 +1,8 @@
 import React, {Component, PropTypes} from 'react'
 import {NavLink, Link} from 'react-router-dom'
-import {
-  Icon, 
-  Button, 
-  ContainerWidth, 
-  Avatar, 
-  IconLabel,
-} from 'egghead-ui'
+import {Icon, Button, Avatar, IconLabel} from 'egghead-ui'
 import {map, isFunction, startsWith} from 'lodash'
-import {navigationWidth, mediumContainerWidth} from 'utils/hardCodedSizes'
+import {navigationWidth} from 'utils/hardCodedSizes'
 import {EggoInstructorBanner} from 'components/Logo'
 
 const sharedLinkClassnames = `
@@ -60,152 +54,139 @@ export default class Navigation extends Component {
     })
   }
 
-  handleWidthChange = () => {
-    if(window.screen.width < mediumContainerWidth) {
-      this.setState({
-        isOpen: false,
-      })
-    } else {
-      this.setState({
-        isOpen: true,
-      })
-    }
-  }
-
   render() {
 
-    const {instructor, items} = this.props
+    const {instructor, items, isLikelyDesktop} = this.props
     const {isOpen} = this.state
 
     return (
-      <ContainerWidth onWidthChange={this.handleWidthChange}>
-        {(screenSize) => (
-          <aside
-            className='bg-base fixed min-vh-100 h-100 z-1 pt2-s'
-            style={{
-              width: navigationWidth,
-              willChange: 'transform',
-              transition: 'transform .3s',
-              left: 0,
-              transform: isOpen ? 'translateX(0)' : 'translateX(-100%)',
-            }}
-          >
+      <aside
+        className='bg-base fixed min-vh-100 h-100 z-1 pt2-s'
+        style={{
+          width: navigationWidth,
+          willChange: 'transform',
+          transition: 'transform .3s',
+          left: 0,
+          transform: isOpen ? 'translateX(0)' : 'translateX(-100%)',
+        }}
+      >
 
-            <div
-              onClick={this.toggle.bind(this)}
-              className={`dn-ns fixed pa2 br2 br--right br--bottom bg-base ${isOpen ? '' : 'o-30'}`}
-              style={{
-                marginLeft: navigationWidth,
-              }}
-            >
-              <Icon
-                type={isOpen ? 'close' : 'menu'}
-                size='3'
+        <div
+          onClick={this.toggle.bind(this)}
+          className={`fixed pa2 br2 br--right br--bottom bg-base ${isOpen ? '' : 'o-30'}`}
+          style={{
+            marginLeft: navigationWidth,
+            display: isLikelyDesktop
+              ? 'none'
+              : 'block'
+          }}
+        >
+          <Icon
+            type={isOpen ? 'close' : 'menu'}
+            size='3'
+            color='white'
+          />
+        </div>
+
+        <div className='pa2 pa3-ns bb tc b--dark-blue'>
+          <EggoInstructorBanner />
+        </div>
+
+        <div className='tc ph3 pt4 pb3 dn db-ns'>
+          <Avatar
+            name={instructor.full_name}
+            url={instructor.avatar_url}
+          />
+          <div className='mt2 white'>
+            {instructor.first_name}
+          </div>
+        </div>
+
+        <nav className={`
+          pv2
+          ${isOpen
+            ? 'flex flex-column'
+            : 'dn'
+          }
+          flex flex-column
+        `}>
+          {map(items, (item, index) => {
+
+            const NavigationLinkContents = () => (
+              <IconLabel
+                iconType={item.iconType}
+                labelText={item.text}
                 color='white'
               />
-            </div>
+            )
 
-            <div className='pa2 pa3-ns bb tc b--dark-blue'>
-              <EggoInstructorBanner />
-            </div>
+            if(isFunction(item.action)) {
+              return (
+                <a
+                  key={index}
+                  className={sharedLinkClassnames}
+                  style={sharedLinkStyle}
+                  onClick={() => {
+                    if(!isLikelyDesktop) {
+                      this.close()
+                    }
+                    item.action()
+                  }}
+                >
+                  <NavigationLinkContents />
+                </a>
+              )
+            }
 
-            <div className='tc ph3 pt4 pb3 dn db-ns'>
-              <Avatar
-                name={instructor.full_name}
-                url={instructor.avatar_url}
-              />
-              <div className='mt2 white'>
-                {instructor.first_name}
-              </div>
-            </div>
+            else {
+              return startsWith(item.action, '/')
+                ? <NavLink
+                    exact
+                    key={index}
+                    className={sharedLinkClassnames}
+                    activeClassName={activeLinkClassnames}
+                    activeStyle={activeLinkStyle}
+                    style={sharedLinkStyle}
+                    onClick={() => {
+                      if(!isLikelyDesktop) {
+                        this.close()
+                      }
+                    }}
+                    to={item.action}
+                  >
+                    <NavigationLinkContents />
+                  </NavLink>
+                : <a
+                    key={index}
+                    className={sharedLinkClassnames}
+                    style={sharedLinkStyle}
+                    onClick={() => {
+                      if(!isLikelyDesktop) {
+                        this.close()
+                      }
+                    }}
+                    href={item.action}
+                  >
+                    <NavigationLinkContents />
+                  </a>
+            }
 
-            <nav className={`
-              pv2
-              ${isOpen
-                ? 'flex flex-column'
-                : 'dn'
-              }
-              flex-ns flex-column-ns
-            `}>
-              {map(items, (item, index) => {
+          })}
+        </nav>
 
-                const NavigationLinkContents = () => (
-                  <IconLabel
-                    iconType={item.iconType}
-                    labelText={item.text}
-                    color='white'
-                  />
-                )
+        <Link 
+          to={'/lessons/new'}
+          className='no-underline mv2 mv4-ns flex justify-center'
+        >
+          <Button 
+            size='small'
+            color='green'
+          >
+            New lesson
+          </Button>
+        </Link>
 
-                if(isFunction(item.action)) {
-                  return (
-                    <a
-                      key={index}
-                      className={sharedLinkClassnames}
-                      style={sharedLinkStyle}
-                      onClick={() => {
-                        if(screenSize === 'small') {
-                          this.close()
-                        }
-                        item.action()
-                      }}
-                    >
-                      <NavigationLinkContents />
-                    </a>
-                  )
-                }
-
-                else {
-                  return startsWith(item.action, '/')
-                    ? <NavLink
-                        exact
-                        key={index}
-                        className={sharedLinkClassnames}
-                        activeClassName={activeLinkClassnames}
-                        activeStyle={activeLinkStyle}
-                        style={sharedLinkStyle}
-                        onClick={() => {
-                          if(screenSize === 'small') {
-                            this.close()
-                          }
-                        }}
-                        to={item.action}
-                      >
-                        <NavigationLinkContents />
-                      </NavLink>
-                    : <a
-                        key={index}
-                        className={sharedLinkClassnames}
-                        style={sharedLinkStyle}
-                        onClick={() => {
-                          if(screenSize === 'small') {
-                            this.close()
-                          }
-                        }}
-                        href={item.action}
-                      >
-                        <NavigationLinkContents />
-                      </a>
-                }
-
-              })}
-            </nav>
-
-            <Link 
-              to={'/lessons/new'}
-              className='no-underline mv2 mv4-ns flex justify-center'
-            >
-              <Button 
-                size='small'
-                color='green'
-              >
-                New lesson
-              </Button>
-            </Link>
-
-          </aside>
-        )}
-      </ContainerWidth>
+      </aside>
     )
   }
 }
